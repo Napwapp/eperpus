9d541bb9c43ccdd09478d7260198e887060ff10a
Napwapp <abduhnawaf@gmail.com>
2025-08-02 22:36:12 +0700

    sedikit setting eslint

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,31 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
       },
       {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-        port: '',
-        pathname: '/**',
+        protocol: "https",
+        hostname: "via.placeholder.com",
+        port: "",
+        pathname: "/**",
       },
     ],
   },
-}
+
+  eslint: {
+    dirs: [
+      "app",
+      "components",
+      "hooks",
+      "lib",
+      "middlewares",
+      "validations",
+      "prisma",
+    ],
+  },
+};
 
 export default nextConfig;
